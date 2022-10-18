68b06d1e273a3c661be451d0e627e8da05bdbc11
Francesca Menichetti <francescamenichetti@Francescas-MacBook-Pro.local>
2022-10-18 17:23:24 +0100

    exercise 1

---
+++ exercise_a_stops.py
@@ -0,0 +1,31 @@
+#Question 1
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+stops.append("Edinburgh Waverley")
+print(stops)
+#Question 2
+stops.insert(0, "Glasgow Queen St")
+print(stops)
+#Question 3
+stops.insert(4, "Polmont")
+print(stops)
+#Question 4
+index = stops.index('Linlithgow')
+print(index)
+#Question 5
+stops.remove('Linlithgow')
+print(stops)
+#Question 6
+stops.pop(2)
+print(stops)
+#Question 7
+num_items = len(stops)
+print(num_items)
+#Question 8
+print(sorted(stops))
+#Question 9
+stops.sort(reverse=True)
+print(stops)
+#Question 10
+for stop in stops:
+    print(stop)
+    
